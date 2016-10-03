0566128a61ed604de32b40b1adb3fdba668385e2
spal7580 <psaineeraj@gmail.com>
2016-10-03 12:01:52 -0600

    linked the updatedViewController.swift file.

---
+++ AfternoonTransitions/untitled folder/updatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  updatedViewController.swift
+//  AfternoonTransitions
+//
+//  Created by Paladugu, Sai neeraj on 10/3/16.
+//  Copyright © 2016 Paladugu, Sai neeraj. All rights reserved.
+//
+
+import UIKit
+
+class updatedViewController : UIViewController
+{
+    
+}
